a718f585eb111d3aa0add5ec673dc1c014dcb240
Henk Bulder <mail@henkbulder.nl>
2013-01-14 21:46:34 +0100

    Update inViewPort.JQuery.js

--- inViewPort.JQuery.js
+++ inViewPort.JQuery.js
@@ -1,38 +1,36 @@
-  	(function($){
-			$.fn.inViewPort = function ( options ) {
-				
-				var settings = $.extend( {
-					
-					'minPercentageInView'	:	100
-					
-				}, options);
-						
-				this.each(function() {
-				
-					var $this = $(this),
-							$win = $(window),
-							isVisible = function () {
-								var min = settings.minPercentageInView / 100;
-										xMin = $this.width() * min;
-										yMin = $this.height() * min;
-										winPosX = $win.scrollLeft() + $win.width(),
-										winPosY = $win.scrollTop() + $win.height(),
-										elPosX = $this.offset().left + xMin,
-										elPosY = $this.offset().top + yMin;
-										
-								if (winPosX > elPosX && winPosY > elPosY) {
-									$this.addClass('in-view');
-									$this.off('vis');
-								}
-							},
-							trg = function () {
-								$this.trigger('vis');
-							}
+(function($){
+  $.fn.inViewPort = function ( options ) {
+
+    var settings = $.extend({
+	  	'minPercentageInView'	:	100			
+	  }, options);
+
+		this.each(function() {
 			
-					$win.on('resize', trg)
-					.on('scroll', trg)
-					.on('ready', trg)
-					.on('vis', isVisible);
-				});
-			};
-		})(jQuery);
+  		var $this = $(this),
+					$win = $(window),
+					isVisible = function () {
+  					var min     = settings.minPercentageInView / 100;
+      					xMin    = $this.width() * min;
+      					yMin    = $this.height() * min;
+			      		winPosX = $win.scrollLeft() + $win.width(),
+					      winPosY = $win.scrollTop() + $win.height(),
+					      elPosX  = $this.offset().left + xMin,
+					      elPosY  = $this.offset().top + yMin;
+
+      			if (winPosX > elPosX && winPosY > elPosY) {
+	  	      	$this.addClass('in-view');
+					    $this.off('vis');
+			      }
+		      },
+		      trg = function () {
+			      $this.trigger('vis');
+      		};
+			
+  		$win.on('resize', trg)
+	 		.on('scroll', trg)
+			.on('ready', trg)
+			.on('vis', isVisible);
+	 	});
+	};
+})(jQuery);
